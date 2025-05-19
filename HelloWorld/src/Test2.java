317ebdc01923e19d1e33f1dd785286cc7322ea63
abhishek975 <dixitabhi1605@gmail.com>
2025-05-20 03:39:24 +0530

    Data Types

---
+++ HelloWorld/src/Test2.java
@@ -0,0 +1,20 @@
+
+public class Test2 {
+public static void main (String args[])
+{
+int a;
+a=30;  //Java is a statically type language we have to tell on compile time only which type of
+//value it is storing.
+//In statically typed languages, the data type of a variable is known at compile time, meaning it must be
+//specified by the programmer 
+//when declaring the variable. This type checking happens before the program runs, allowing the compiler 
+//to catch errors related to incorrect data types, ensuring more robust code
+System.out.println(a);
+String name="Abhishek";
+System.out.println(name);
+
+//Data type in Java 
+// Primitive data types (Inbuilt data types,Fixed Size and Fixed Range,8 data types are there)
+//
+}
+}
