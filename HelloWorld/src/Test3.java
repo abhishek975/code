8397068317d1167a3084ca78bae301d641f1fd8e
abhishek975 <dixitabhi1605@gmail.com>
2025-05-20 17:08:42 +0530

    Type Casting

---
+++ HelloWorld/src/Test3.java
@@ -0,0 +1,15 @@
+
+public class Test3 {
+	public static void main(String args[]) {
+ //Type Casting example
+//Explicit Type and Implicit Type Conversion
+	int i=50;
+	double d=i; //Implicit Conversion
+	System.out.println(i);
+	System.out.println(d);
+	
+	double g=67.789;
+	 char t=(char)g; //Explicit Conversion
+	 System.out.println(t);
+	}
+}
