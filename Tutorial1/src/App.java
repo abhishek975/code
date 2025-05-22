4199d8a5d368f82837c4761b78afe5e58853e010
abhishek975 <dixitabhi1605@gmail.com>
2025-05-22 22:50:16 +0530

    Array of Strings

---
+++ Tutorial1/src/App.java
@@ -0,0 +1,19 @@
+
+public class App {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+   String[] words=new String[3];
+      words[0]="Hello";
+      words[1]="to";
+      words[2]="you";
+      System.out.println(words[2]);
+      String[] fruits= {"apple","orange","banana"};
+      for(String fruit:fruits)
+      {
+    	  System.out.println(fruit);
+      }
+    		 
+	}
+
+}
