8a16a5989a3163a9b76d342d755450ee0037458a
abhishek975 <dixitabhi1605@gmail.com>
2025-05-24 13:35:54 +0530

    Difference between Local and Instance Variables

--- Tutorial6/src/App.java
+++ Tutorial6/src/App.java
@@ -3,8 +3,8 @@ class Frog{
 	String name;
 	public void setAge(int age) { //local variables with same name  as of instance variables closest to 
 		//function are given preferences
-		
-		System.out.println(this.age);  // By using this keyword, we are referring to the instance variables
+		this.age=40;
+		System.out.println(age);  // By using this keyword, we are referring to the instance variables
 		//of current object
 	}
 }
@@ -13,8 +13,10 @@ public class App {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
        Frog f1=new Frog();
-       f1.age=30;
+//       f1.age=30;
+       System.out.println("Local Variable age is ");
        f1.setAge(10);
+       System.out.println("Instance Variable age is "+ f1.age);
 	}
 
 }
