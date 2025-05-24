eb361fcc34c36074d1c143e4b1db49405841f267
abhishek975 <dixitabhi1605@gmail.com>
2025-05-24 13:31:44 +0530

    Use of this keyword

--- Tutorial6/src/App.java
+++ Tutorial6/src/App.java
@@ -3,7 +3,9 @@ class Frog{
 	String name;
 	public void setAge(int age) { //local variables with same name  as of instance variables closest to 
 		//function are given preferences
-		System.out.println(age);
+		
+		System.out.println(this.age);  // By using this keyword, we are referring to the instance variables
+		//of current object
 	}
 }
 public class App {
