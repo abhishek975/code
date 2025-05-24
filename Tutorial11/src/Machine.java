c7cd45aaf166a5764f7bdd7ede5f74c81571b737
abhishek975 <dixitabhi1605@gmail.com>
2025-05-24 17:43:55 +0530

    Accessiblity Mode in Inheritance

--- Tutorial11/src/Machine.java
+++ Tutorial11/src/Machine.java
@@ -1,6 +1,7 @@
 
 public class Machine {
-	
+	private int code;//Private type members of a class is not accessible by other class members
+	protected String name;//Protected type members of a class are accessible only by child class members
 	public void start()
 {  System.out.println("Machine Started");
 } 
