f615dcbf3549ccb5bcfde626d6293aad357fa8c6
abhishek975 <dixitabhi1605@gmail.com>
2025-05-27 08:25:18 +0530

    Upcasting And DownCasting

---
+++ Tutorial17/src/App.java
@@ -0,0 +1,40 @@
+class Machine{
+	public void start() {
+		System.out.println("Machine Started");
+	}
+	
+}
+class Camera extends Machine {
+	public void start() {
+		System.out.println("Camera Started");
+		
+	}
+	public void snap() {
+		System.out.println("Photo Taken");
+	}
+}
+public class App {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        Machine machine1=new Machine();
+        Camera camera1=new Camera();
+        machine1.start();
+        camera1.start();
+        
+        //Upcasting
+        Machine machine2=new Camera();
+        machine2.start();
+     //   machine2.snap(); Won't work because with the help of Machine type reference variables 
+        //we can access only methods defined within the Machine(Parent) class
+		
+        //Downcasting
+        Machine machine3=new Camera();
+        Camera camera2=(Camera)machine3;
+		camera2.snap();
+		camera2.start();
+		
+		
+	}
+
+}
